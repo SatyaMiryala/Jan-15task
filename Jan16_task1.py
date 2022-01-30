c6a622ed6b5c3bb97c8493dbea7132edc653c76b
SatyaMiryala <98235590+SatyaMiryala@users.noreply.github.com>
2022-01-30 15:23:28 +0530

    Add files via upload

---
+++ Jan16_task1.py
@@ -0,0 +1,18 @@
+#write a program to calculate area of circle with radius (float) (collect using input function)
+
+pi = 3.14
+
+r = float(input("please enter the Radius"))
+
+area = pi *(r **2)
+print("The area of the circle with raduis {} is {}".format(r, area))
+
+#write a program to calculate perimeter of the cricle with radius (float) (collect it through input function)
+pi = 3.14
+
+r = float(input("please enter the Radius"))
+
+circle_perimeter = 2* pi *r
+print("The perimeter of the circle with raduis {} is {}".format(r, circle_perimeter))
+
+
