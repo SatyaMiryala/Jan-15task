9b0da444e2fadc3464a4be61a22ed99c8ebb4580
SatyaMiryala <98235590+SatyaMiryala@users.noreply.github.com>
2022-01-30 20:06:01 +0530

    Add files via upload

---
+++ String indexing and slicing type 1&2.py
@@ -0,0 +1,120 @@
+# String indexing and sciling
+
+var = "computer_science"
+
+print(var[8])
+print(var[-8])
+
+#String Slicing
+
+# Type1
+
+
+#val ="computer_science"
+
+'''
+type 1
+
+print(val[3:10])	ormatio
+print(val[2:5])	for
+print(val[0:10])	informatio
+print(val[5:-5])	matoin*techn
+print(val[2:8])	format
+print(val[:8])	informat
+print(val[5:])	mation*Technology
+print(val[8:12])	ion*
+print(val[:])	Information*Technology
+print(val[12:5])	empty
+
+type 2
+
+print(val[3:10:1])	ormatio
+print(val[3:10:2])	omto
+print(val[3:10:3])	oao
+print(val[5:-5:1])	matoin*techn
+print(val[5:-5:2])	mti*eh
+print(val[5:-5:5])	mnn
+print(val[:8:4])	iri
+print(val[5::3])	mi*cog
+print(val[::2])	ifraintcnlg
+print(val[12:5:3])	empty
+
+'''
+
+val = "Information*Technology"
+print(val[3:10])
+print(val[2:5])
+print(val[0:10])
+print(val[5:-5])
+print(val[2:8])
+print(val[:8])
+print(val[5:])
+print(val[8:12])
+print(val[:])
+print(val[12:5])
+
+
+print(val[3:10:1])
+print(val[3:10:2])
+print(val[3:10:3])
+print(val[5:-5:1])
+print(val[5:-5:2])
+print(val[5:-5:5])
+print(val[:8:4])
+print(val[5::3])
+print(val[::2])
+print(val[12:5:3])
+
+
+'''
+type 1	
+print(val[3:10])	hon_int
+print(val[2:5])	tho
+print(val[0:10])	python_int
+print(val[5:-5])	n_interp
+print(val[2:8])	thon_i
+print(val[:8])	python_i
+print(val[5:])	n_interpreter
+print(val[8:12])	nter
+print(val[:])	Python_Interpreter
+print(val[12:5])	empty
+	
+Type 2	
+print(val[3:10:1])	hon_int
+print(val[3:10:2])	hnit
+print(val[3:10:3])	h_t
+print(val[5:-5:1])	n_interp
+print(val[5:-5:2])	nitr
+print(val[5:-5:5])	ne
+print(val[:8:4])	po
+print(val[5::3])	nnrer
+print(val[::2])	pto_nepee
+print(val[12:5:3])	empty
+'''
+
+val = "Python_Interpreter"
+
+
+print(val[3:10])
+print(val[2:5])
+print(val[0:10])
+print(val[5:-5])
+print(val[2:8])
+print(val[:8])
+print(val[5:])
+print(val[8:12])
+print(val[:])
+print(val[12:5])
+
+
+print(val[3:10:1])
+print(val[3:10:2])
+print(val[3:10:3])
+print(val[5:-5:1])
+print(val[5:-5:2])
+print(val[5:-5:5])
+print(val[:8:4])
+print(val[5::3])
+print(val[::2])
+print(val[12:5:3])
+
