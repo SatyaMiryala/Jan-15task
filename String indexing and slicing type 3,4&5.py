59f3804acff5ca2b0bb027adf07a71f3562bbf08
SatyaMiryala <98235590+SatyaMiryala@users.noreply.github.com>
2022-01-31 17:17:42 +0530

    Add files via upload

---
+++ String indexing and slicing type 3,4&5.py
@@ -0,0 +1,163 @@
+#String indexing and slicing
+
+#Type 3
+
+a = "Information_Technology"
+
+'''
+[-2:0:-1]	golonhcet*noitamrofni
+#[5:-5:5]	mnh
+#[-5:5:-5]	ott
+#[5:-5:-5]	empty
+#[3:9:-1]	empty
+#[9:-10:-2]	empty
+#[2:-4:-4]	empty
+#[-4:2:-2]	lcna
+#[0:-1:-3]	empty
+#[-3:3:6]	empty
+#[::-3]	ylhtoaoi
+#[::-2]	yoohe*otmon
+#[2:-2:-2]	empty
+#[2:-2:2]	fraintcnl
+#[5:-5:5]	mnh
+#[3:-3:3]	oaothl
+#[::2]	ifraintcnlg
+#[::-5]	yn*an
+
+'''
+
+print(a[-2:0:-1])
+print(a[5:-5:5])
+print(a[-5:5:-5])
+print(a[5:-5:-5])
+print(a[3:9:-1])
+print(a[9:-10:-2])
+print(a[2:-4:-4])
+print(a[-4:2:-2])
+print(a[0:-1:-3])
+print(a[-3:3:6])
+print(a[::-3])
+print(a[::-2])
+print(a[2:-2:-2])
+print(a[2:-2:2])
+print(a[5:-5:5])
+print(a[3:-3:3])
+print(a[::2])
+print(a[::-5])
+
+a= "python_is_using_interpreter"
+
+'''[-2:0:-1]	eterpretni_gnisu_si_nohty
+#[5:-5:5]	nu_r
+#[-5:5:-5]	rnii
+#[5:-5:-5]	empty
+#[3:9:-1]	empty
+#[9:-10:-2]	empty
+#[2:-4:-4]	empty
+#[-4:2:-2]	epen_ns_inh
+#[0:-1:-3]	empty
+#[-3:3:6]	empty
+#[::-3]	rerngssnt
+#[::-2]	rtrrtigius_otp
+#[2:-2:-2]	empty
+#[2:-2:2]	to_suigitrrt
+#[5:-5:5]	nu_r
+#[3:-3:3]	h__i_tp
+#[::2]	pto_suiitrrtr
+#[::-5]	rpis_y
+'''
+
+print(a[-2:0:-1])
+print(a[5:-5:5])
+print(a[-5:5:-5])
+print(a[5:-5:-5])
+print(a[3:9:-1])
+print(a[9:-10:-2])
+print(a[2:-4:-4])
+print(a[-4:2:-2])
+print(a[0:-1:-3])
+print(a[-3:3:6])
+print(a[::-3])
+print(a[::-2])
+print(a[2:-2:-2])
+print(a[2:-2:2])
+print(a[5:-5:5])
+print(a[3:-3:3])
+print(a[::2])
+print(a[::-5])
+
+
+a = "computer science"
+
+'''print(a[-10:10:3])	es
+print(a[-5:-1:1])	ienc
+print(a[-8:-2:10])	 
+print(a[5::3])	empty
+print(a[:5:2])	cmu
+print(a[10:14:2])	ce
+print(a[-10:-3:3])	ese
+print(a[5:12:5])	tc
+print(a[3:-3:3])	pese
+print(a[-5:-3:20])	i
+'''
+
+print(a[-10:10:3])
+print(a[-5:-1:1])
+print(a[-8:-2:10])
+print(a[5::3])
+print(a[:5:2])
+print(a[10:14:2])
+print(a[-10:-3:3])
+print(a[5:12:5])
+print(a[3:-3:3])
+print(a[-5:-3:20])
+
+
+a = "Information_technology"
+
+'''print(a[-10:10:3])	empty
+print(a[-5:-1:1])	olog
+print(a[-8:-2:10])	c
+print(a[5::3])	mi_cog
+print(a[:5:2])	ifr
+print(a[10:14:2])	nt
+print(a[-10:-3:3])	thl
+print(a[5:12:5])	mn
+print(a[3:-3:3])	oaothl
+print(a[-5:-3:20])	o
+'''
+print(a[-10:10:3])
+print(a[-5:-1:1])
+print(a[-8:-2:10])
+print(a[5::3])
+print(a[:5:2])
+print(a[10:14:2])
+print(a[-10:-3:3])
+print(a[5:12:5])
+print(a[3:-3:3])
+print(a[-5:-3:20])
+
+
+a = "Google_chrome_browser"
+
+'''print(a[-10:10:3])	empty
+print(a[-5:-1:1])	owse
+print(a[-8:-2:10])	_
+print(a[5::3])	ehmbwr
+print(a[:5:2])	gol
+print(a[10:14:2])	oe
+print(a[-10:-3:3])	mbw
+print(a[5:12:5])	eo
+print(a[3:-3:3])	g_rer
+print(a[-5:-3:20])	o
+'''
+print(a[-10:10:3])
+print(a[-5:-1:1])
+print(a[-8:-2:10])
+print(a[5::3])
+print(a[:5:2])
+print(a[10:14:2])
+print(a[-10:-3:3])
+print(a[5:12:5])
+print(a[3:-3:3])
+print(a[-5:-3:20])
